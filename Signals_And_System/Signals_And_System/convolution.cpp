3dd4a4bdbfcf9f26766f11694ba5d343ce7d987c
YSCHOI <shipgo2@gmail.com>
2022-01-28 01:41:02 +0900

    Convolution

---
+++ Signals_And_System/Signals_And_System/convolution.cpp
@@ -0,0 +1,7 @@
+#include "convolution.h"
+
+template <class Ans, class Input_1, class Input_2>
+std::vector<Ans> conv(std::vector<Input_1>& x, std::vector<Input_2>& h) 
+{
+	return;
+}
---
+++ Signals_And_System/Signals_And_System/convolution.h
@@ -0,0 +1,7 @@
+//this function is about convnolution two vec x[n], h[n]
+
+#include <math.h>
+#include <vector>
+
+template <class Ans, class Input_1, class Input_2>
+std::vector<Ans> conv(std::vector<Input_1>& x, std::vector<Input_2>& h);
